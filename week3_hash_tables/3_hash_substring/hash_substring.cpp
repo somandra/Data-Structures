a547a4aaa98a8c40961bf9a4692d4d190fd7fe8b
somandra <somandra.bhl2015@gmail.com>
2020-08-30 13:16:46 +0530

    implemented Rabin Karp Algorithm and substring equality using hashing

--- week3_hash_tables/3_hash_substring/hash_substring.cpp
+++ week3_hash_tables/3_hash_substring/hash_substring.cpp
@@ -3,20 +3,23 @@
 #include <vector>
 
 using std::string;
+
+#define MAXCHAR 256
+
 typedef unsigned long long ull;
 
 struct Data {
     string pattern, text;
 };
 
-size_t PolyHash(const string& s){
-    static const size_t multiplier = 263;
-    static const size_t prime = 100003;
-    unsigned long long hash = 0;
-    for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
-        hash = (hash * multiplier + s[i]) % prime;
-    return hash;
-}
+// size_t PolyHash(const string& s){
+//     static const size_t multiplier = 256;
+//     static const size_t prime = 101;
+//     unsigned long long hash = 0;
+//     for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
+//         hash = (hash * multiplier + s[i]) % prime;
+//     return hash;
+// }
 
 Data read_input() {
     Data data;
@@ -39,44 +42,87 @@ std::vector<int> get_occurrences(const Data& input) {
     return ans;
 }
 
-std::vector<size_t> PreComputeHash(const string& t,size_t len_t, size_t len_p){
-    static const size_t multiplier = 263;
-    static const size_t prime = 100003;
-    size_t n = len_t-len_p+1;
-    std::vector<size_t> H(n);
-    string s = t.substr(len_t-len_p);
-    H[len_t-len_p] = PolyHash(s);
-    unsigned long long y = 1;
-    for (size_t i = 1; i <=len_p; i++)
-    {
-        y = ((y*multiplier)%prime+prime)%prime;
-    }
+// std::vector<int> PreComputeHash(const string& t,size_t len_t, size_t len_p){
+//     static const size_t multiplier = 256;
+//     static const size_t prime = 101;
+//     unsigned long long int n = len_t-len_p+1;
+//     std::vector<int> H(n);
+//     string s = t.substr(len_t-len_p);
+//     H[len_t-len_p] = PolyHash(s);
+//     unsigned long long y = 1;
+//     for (size_t i = 1; i <=len_p; i++)
+//     {
+//         y = (((y*multiplier)%prime)+prime)%prime;
+//     }
+//     std::cout<<y<<"\n";
+//     for (int i = len_t-len_p-1; i >=0; i--)
+//     {
+//         H[i] = (multiplier*H[i+1]+t[i]-y*t[i+len_p-1])%prime;
+//         std::cout<<H[i+1]<<" "<<t[i]<<" "<<H[i]<<" "<<t[i+len_p]<<"\n";
+//     }
 
-    for (size_t i = len_t-len_p-1; i >=0; i--)
-    {
-        H[i] = ((multiplier*H[i+1]+t[i]-y*t[i+len_p])%prime+prime)%prime;
-    }
+//     return H;
+// }
 
-    return H;
-}
+// std::vector<int> RabinKarp(const Data& input){
+//     const string& p = input.pattern, t = input.text;
+//     std::vector<int> result;
+//     size_t pHash = PolyHash(p);
+//     //std::cout<<pHash<<" "<<p.length()<<"\n";
+//     std::vector<int> H = PreComputeHash(t,t.length(),p.length());
+//     for(int i=0;i<=t.length()-p.length(); i++){
+//         //size_t tHash = PolyHash(t.substr(i,p.length()));
+//         //std::cout<<tHash<<" "<<t.substr(i,p.length())<<"\n";
+//         std::cout<<pHash<<" "<<H[i]<<"\n";
+//         if(pHash!=H[i]){
+//             continue;
+//         }
+//         if(t.substr(i,p.length()).compare(p)==0){
+//             result.push_back(i);
+//         }
+//     }
+//     return result;
+// }
 
-std::vector<int> RabinKarp(const Data& input){
-    const string& p = input.pattern, t = input.text;
-    std::vector<int> result;
-    size_t pHash = PolyHash(p);
-    //std::cout<<pHash<<" "<<p.length()<<"\n";
-    //std::vector<size_t> H = PreComputeHash(t,t.length(),p.length());
-    for(int i=0;i<=t.length()-p.length(); i++){
-        size_t tHash = PolyHash(t.substr(i,p.length()));
-        //std::cout<<tHash<<" "<<t.substr(i,p.length())<<"\n";
-        if(pHash!=tHash){
-            continue;
-        }
-        if(t.substr(i,p.length()).compare(p)==0){
+
+std::vector<int> RabinKarp(const Data& input) {
+    string pattern = input.pattern;
+    string mainString = input.text;
+    int prime = 101;
+   int patLen = pattern.size();
+   int strLen = mainString.size();
+   int charIndex, pattHash = 0, strHash = 0, h = 1;
+   std::vector<int> result;
+
+   for(int i = 0; i<patLen-1; i++) {
+      h = (h*MAXCHAR) % prime;    //calculating h = {d^(M-1)} mod prime
+   }
+   
+   for(int i = 0; i<patLen; i++) {
+      pattHash = (MAXCHAR*pattHash + pattern[i]) % prime;    //pattern hash value
+      strHash = (MAXCHAR*strHash + mainString[i]) % prime;   //hash for first window
+   }
+   
+   for(int i = 0; i<=(strLen-patLen); i++) {
+      if(pattHash == strHash) {      //when hash values are same check for matching
+         for(charIndex = 0; charIndex < patLen; charIndex++) {
+            if(mainString[i+charIndex] != pattern[charIndex])
+               break;
+         }
+
+         if(charIndex == patLen) {    //the pattern is found
             result.push_back(i);
-        }
-    }
-    return result;
+         }
+      }
+
+      if(i < (strLen-patLen)) {    //find hash value for next window
+         strHash = (MAXCHAR*(strHash - mainString[i]*h) + mainString[i+patLen])%prime;
+         if(strHash < 0) {
+            strHash += prime;    //when hash value is negative, make it positive
+         }
+      }
+   }
+   return result;
 }
 
 
--- week3_hash_tables/4_substring_equality/substring_equality.cpp
+++ week3_hash_tables/4_substring_equality/substring_equality.cpp
@@ -1,17 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+#define m 1000000009
+#define p 263
 
-class Solver {
-	string s;
-public:	
-	Solver(string s) : s(s) {	
-		// initialization, precalculation
-	}
-	bool ask(int a, int b, int l) {
-		return s.substr(a, l) == s.substr(b, l);
-	}
-};
 
 int main() {
 	ios_base::sync_with_stdio(0), cin.tie(0);
@@ -19,10 +11,45 @@ int main() {
 	string s;
 	int q;
 	cin >> s >> q;
-	Solver solver(s);
-	for (int i = 0; i < q; i++) {
-		int a, b, l;
-		cin >> a >> b >> l;
-		cout << (solver.ask(a, b, l) ? "Yes\n" : "No\n");
+	vector<long long> p_pow (s.length());
+	p_pow[0] = 1;
+	for(size_t i=1; i<p_pow.size(); ++i){
+    	p_pow[i] = (m + (p_pow[i-1] * p) % m) % m;
 	}
+	vector<long long> h (s.length());
+	for (size_t i=0; i<s.length(); ++i){
+		h[i] = (m + (s[i] - 'a' + 1) * p_pow[i] % m) % m;
+		if(i){
+			h[i] = (m + (h[i] + h[i-1]) % m) % m;
+		}
+	}
+	vector<string> result;
+	for (int i = 0; i < q; i++) {
+		int a,b,len;
+		cin >> a >> b >> len;
+		if(a == b){      // just avoid extra calculation, saves little time
+			result.push_back("Yes");
+		}else{
+			long long h1 = h[a+len-1] % m;
+			if(a){
+				h1 = (m + (h1 - h[a-1]) % m) % m;
+			}
+			long long h2 = h[b+len-1] % m;
+			if(b){
+				h2 = (m + (h2 - h[b-1]) % m) % m;
+			}
+			if (a < b && h1 * p_pow[b-a] % m == h2 % m || a > b && h1 % m == h2 * p_pow[a-b] % m){
+				//cout << "Yes" << endl;
+				result.push_back("Yes");
+			}else{
+				// cout << "No" << endl;
+				result.push_back("No");
+			}
+		}
+	}
+	for (size_t i = 0; i < result.size(); i++)
+	{
+		cout<<result[i]<<"\n";
+	}
+	
 }
